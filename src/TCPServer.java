d6c6dce9b3734b19554cccf4301b3546e4413467
sodiee <mathi.carlsen@gmail.com>
2023-09-04 10:54:33 +0200

    Specifikation

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -22,25 +22,25 @@ public class TCPServer {
         DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("En anden klient forsøger at sende en besked. Vil du chatte med dem? (ja/nej)");
+        String response = scanner.nextLine();
 
+        if (response.equalsIgnoreCase("nej")) {
+            System.out.println("Okay, du vil ikke chatte lige nu. Lukker forbindelsen.");
+            connectionSocket.close();
+        } else if (response.equalsIgnoreCase("ja")) {
         while (true) {
-            System.out.println("En anden klient forsøger at sende en besked. Vil du chatte med dem? (ja/nej)");
-            String response = scanner.nextLine();
 
-            if (response.equalsIgnoreCase("nej")) {
-                System.out.println("Okay, du vil ikke chatte lige nu. Lukker forbindelsen.");
-                connectionSocket.close();
-                break;
-            } else if (response.equalsIgnoreCase("ja")) {
                 System.out.print("Skriv en besked til klienten: ");
                 serverInput = scanner.nextLine();
                 outToClient.writeBytes(serverInput + '\n');
 
                 clientSentence = inFromClient.readLine();
                 System.out.println("FROM OTHER CLIENT: " + clientSentence);
-            } else {
-                System.out.println("Svar med 'ja' eller 'no'.");
-            }
+
+        }
+        } else {
+            System.out.println("Svar med 'ja' eller 'nej'.");
         }
 
         welcomeSocket.close();
