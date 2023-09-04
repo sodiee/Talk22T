84702ebfc95f69e20e074a8e16d71d95d0aea07f
sodiee <mathi.carlsen@gmail.com>
2023-09-04 09:57:06 +0200

    Specifikation

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -14,7 +14,7 @@ public class TCPClient {
 		
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		
-		Socket clientSocket = new Socket("10.10.131.139", 6789);
+		Socket clientSocket = new Socket("localhost", 6789);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
