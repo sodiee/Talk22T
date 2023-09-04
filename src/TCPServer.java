2627c26b05290b489ad0b645f4cd849d6e563a9a
sodiee <mathi.carlsen@gmail.com>
2023-09-04 10:51:39 +0200

    Specifikation

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -11,33 +11,39 @@ public class TCPServer {
 
         String clientSentence;
         String serverInput;
-        ServerSocket welcomSocket = new ServerSocket(6789);
 
-        Socket connectionSocket = welcomSocket.accept();
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        System.out.println("Venter på klientforbindelse...");
+        Socket connectionSocket = welcomeSocket.accept();
+        System.out.println("Klient forbundet.");
+
         BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
         DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
         Scanner scanner = new Scanner(System.in);
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-
         while (true) {
+            System.out.println("En anden klient forsøger at sende en besked. Vil du chatte med dem? (ja/nej)");
+            String response = scanner.nextLine();
 
-            System.out.println("En anden klient forsøger at koble til dit chatrum. Vil du chatte med dem?");
-            if (scanner.nextLine().equals("no")) {
-                System.out.println("Okay, du vil ikke chatte lige nu.");
-                //welcomSocket.close();
+            if (response.equalsIgnoreCase("nej")) {
+                System.out.println("Okay, du vil ikke chatte lige nu. Lukker forbindelsen.");
+                connectionSocket.close();
                 break;
-            } else {
-                //welcomSocket.accept();
+            } else if (response.equalsIgnoreCase("ja")) {
+                System.out.print("Skriv en besked til klienten: ");
+                serverInput = scanner.nextLine();
+                outToClient.writeBytes(serverInput + '\n');
+
                 clientSentence = inFromClient.readLine();
                 System.out.println("FROM OTHER CLIENT: " + clientSentence);
-                serverInput = bufferedReader.readLine();
-                outToClient.writeBytes(serverInput);
+            } else {
+                System.out.println("Svar med 'ja' eller 'no'.");
             }
         }
 
+        welcomeSocket.close();
     }
 
 }
